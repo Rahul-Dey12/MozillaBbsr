bd0f9fc514ca6eefa9df161de6451f73eeff4b30
Rahul-Dey12 <r.dey711@gamil.com>
2019-03-03 21:29:31 +0530

    cardview is now clickable

--- app/src/main/java/ml/mozillabbsr/www/mozillabbsr/DataAdapter.java
+++ app/src/main/java/ml/mozillabbsr/www/mozillabbsr/DataAdapter.java
@@ -1,6 +1,8 @@
 package ml.mozillabbsr.www.mozillabbsr;
 
 import android.content.Context;
+import android.content.Intent;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -17,11 +19,11 @@ import java.util.List;
 public class DataAdapter extends RecyclerView.Adapter<mViewHolder>{
 
     Context mcontext;
-    List<Data> meventdataList;
+    List<Data> mDataList;
 
     DataAdapter(Context mContext, List<Data> dataList) {
         this.mcontext = mContext;
-        this.meventdataList = dataList;
+        this.mDataList = dataList;
     }
 
     @Override
@@ -31,17 +33,28 @@ public class DataAdapter extends RecyclerView.Adapter<mViewHolder>{
     }
     @Override
     public void onBindViewHolder(final mViewHolder holder, int position) {
-        holder.mImage.setImageResource(meventdataList.get(position).getImgurl());
-        holder.mTitle.setText(meventdataList.get(position).getTitle());
+        holder.mImage.setImageResource(mDataList.get(position).getImgurl());
+        holder.mTitle.setText(mDataList.get(position).getTitle());
+        holder.mCardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent mIntent = new Intent(mcontext, DetailActivity.class);
+                mIntent.putExtra("Title",mDataList.get(holder.getAdapterPosition()).getTitle());
+                //mIntent.putExtra("Description",mDataList.get(holder.getAdapterPosition()).ge);
+                mIntent.putExtra("Image", mDataList.get(holder.getAdapterPosition()).getImgurl());
+                mcontext.startActivity(mIntent);
+            }
+        });
     }
     @Override
     public int getItemCount() {
-        return meventdataList.size();
+        return mDataList.size();
     }
 }
 class mViewHolder extends RecyclerView.ViewHolder {
     ImageView mImage;
     TextView mTitle;
+    CardView mCardView = itemView.findViewById(R.id.card_view);
     mViewHolder(View itemView) {
 
         super(itemView);
---
+++ app/src/main/java/ml/mozillabbsr/www/mozillabbsr/DetailActivity.java
@@ -0,0 +1,26 @@
+package ml.mozillabbsr.www.mozillabbsr;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class DetailActivity extends AppCompatActivity {
+    ImageView mFlower;
+    TextView mTitle;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+
+        mFlower = findViewById(R.id.ivImage);
+        mTitle = findViewById(R.id.tvDescription);
+
+        Bundle mBundle = getIntent().getExtras();
+        if (mBundle != null) {
+            mFlower.setImageResource(mBundle.getInt("Image"));
+            mTitle.setText(mBundle.getString("Title"));
+        }
+    }
+}
