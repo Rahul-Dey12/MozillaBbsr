852b006a12c3f6043483a6c29f58508c072f49c6
bibhutipanda <1705030@kiit.ac.in>
2019-03-04 20:33:14 +0530

    Merging job done

---
+++ app/src/main/java/ml/mozillabbsr/www/mozillabbsr/SlideAdapter.java
@@ -0,0 +1,54 @@
+package ml.mozillabbsr.www.mozillabbsr;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v4.view.PagerAdapter;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+import ml.mozillabbsr.www.mozillabbsr.R;
+
+public class SlideAdapter extends PagerAdapter {
+
+    LayoutInflater layoutInflater;
+    Context context;
+
+    public SlideAdapter(Context context){
+        this.context=context;
+    }
+
+    public int[] lst_images={
+            R.drawable.crowdsource,
+            R.drawable.github,
+            R.drawable.hackathon
+    };
+    @Override
+    public int getCount() {
+        return lst_images.length;
+    }
+
+    @Override
+    public boolean isViewFromObject(@NonNull View view, @NonNull Object o) {
+        return (view==(LinearLayout)o);
+    }
+
+    @NonNull
+    @Override
+    public Object instantiateItem(@NonNull ViewGroup container, int position) {
+        layoutInflater=(LayoutInflater)context.getSystemService(context.LAYOUT_INFLATER_SERVICE);
+        View view=layoutInflater.inflate(R.layout.slide,container,false);
+        LinearLayout layoutslide=(LinearLayout)view.findViewById(R.id.slidelinearlayout);
+        ImageView imgslide=(ImageView)view.findViewById(R.id.slideimg);
+        imgslide.setImageResource(lst_images[position]);
+        container.addView(view);
+        return view;
+    }
+
+    @Override
+    public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
+        container.removeView((LinearLayout)object);
+    }
+}
