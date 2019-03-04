f34922e6e1f5094aea30d2c2b9335c9ddde767d7
Rahul-Dey12 <r.dey711@gamil.com>
2019-03-05 02:08:28 +0530

    bottom navigation view updated

--- app/src/main/java/ml/mozillabbsr/www/mozillabbsr/HomeFragment.java
+++ app/src/main/java/ml/mozillabbsr/www/mozillabbsr/HomeFragment.java
@@ -7,6 +7,7 @@ import android.support.v4.app.Fragment;
 import android.support.v4.view.ViewPager;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,52 +16,45 @@ import java.util.ArrayList;
 
 
 public class HomeFragment extends Fragment {
-    RecyclerView upRecyclerView;
-    RecyclerView psRecyclerView;
-    Data upData;
-    Data psData;
-    ArrayList<Data> upDataList;
-    ArrayList<Data> psDataList;
-
+    RecyclerView mRecyclerView;
+    Data mData;
+    ArrayList<Data> mDataList;
+    Toolbar myToolbar;
+    ViewPager viewpager;
+    SlideAdapter myadapter;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_home, container, false);
-        upDataList = new ArrayList<Data>();
-        upRecyclerView = view.findViewById(R.id.up_recyclerview);
+        View mview = inflater.inflate(R.layout.fragment_home2, container, false);
+
+        viewpager=mview.findViewById(R.id.viewpager);
+
+        myadapter=new SlideAdapter(getActivity());
+        viewpager.setAdapter(myadapter);
+
+        mDataList = new ArrayList<Data>();
+        mRecyclerView = mview.findViewById(R.id.up_recyclerview);
         GridLayoutManager upGridLayoutManager = new GridLayoutManager(getActivity(), 2);
-        upRecyclerView.setLayoutManager(upGridLayoutManager);
+        mRecyclerView.setLayoutManager(upGridLayoutManager);
 
-        upData =new Data(R.drawable.hackathon,"Hackathon");
-        upDataList.add(upData);
-        upData =new Data(R.drawable.recuritment,"recuritment");
-        upDataList.add(upData);
-        upData =new Data(R.drawable.webvr,"WebVr");
-        upDataList.add(upData);
-        upData =new Data(R.drawable.rust,"Rust");
-        upDataList.add(upData);
-        upData =new Data(R.drawable.googlemini,"Googlemini");
-        upDataList.add(upData);
+        mData =new Data(R.drawable.hackathon,"Hackathon");
+        mDataList.add(mData);
+        mData =new Data(R.drawable.recuritment,"recuritment");
+        mDataList.add(mData);
+        mData =new Data(R.drawable.webvr,"WebVr");
+        mDataList.add(mData);
 
-        DataAdapter upAdapter=new DataAdapter(getActivity(), upDataList);
-        upRecyclerView.setAdapter(upAdapter);
 
-        /*psDataList = new ArrayList<Data>();
-        psRecyclerView = view.findViewById(R.id.ps_recyclerview);
-        GridLayoutManager psGridLayoutManager = new GridLayoutManager(getActivity(), 2);
-        psRecyclerView.setLayoutManager(psGridLayoutManager);
+        mData =new Data(R.drawable.github,"OpenSource");
+        mDataList.add(mData);
+        mData =new Data(R.drawable.rust,"Rust");
+        mDataList.add(mData);
+        mData =new Data(R.drawable.googlemini,"Googlemini");
+        mDataList.add(mData);
 
-        psData =new Data(R.drawable.github,"OpenSource");
-        psDataList.add(psData);
-        psData =new Data(R.drawable.rust,"Rust");
-        psDataList.add(psData);
-        psData =new Data(R.drawable.googlemini,"Googlemini");
-        psDataList.add(psData);
-
-        DataAdapter psAdapter=new DataAdapter(getActivity(), psDataList);
-        psRecyclerView.setAdapter(psAdapter);*/
-        return view;
+        DataAdapter upAdapter=new DataAdapter(getActivity(), mDataList);
+        mRecyclerView.setAdapter(upAdapter);
+        return mview;
     }
 }
--- app/src/main/java/ml/mozillabbsr/www/mozillabbsr/MainActivity.java
+++ app/src/main/java/ml/mozillabbsr/www/mozillabbsr/MainActivity.java
@@ -2,43 +2,32 @@ package ml.mozillabbsr.www.mozillabbsr;
 
 import android.support.annotation.NonNull;
 import android.support.design.bottomappbar.BottomAppBar;
-import android.support.design.widget.NavigationView;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentTransaction;
-import android.support.v4.view.GravityCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.view.MenuItem;
 
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity {
-    RecyclerView upRecyclerView;
-    RecyclerView psRecyclerView;
-    Data upData;
-    Data psData;
-    ArrayList<Data> upDataList;
-    ArrayList<Data> psDataList;
-    Toolbar myToolbar;
-    ViewPager viewpager;
-    SlideAdapter myadapter;
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
+
 
     private DrawerLayout drawerLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // Viewpager code:
-        viewpager=findViewById(R.id.viewpager);
 
-        myadapter=new SlideAdapter(this);
-        viewpager.setAdapter(myadapter);
+
+        // Viewpager code:
+
 
 
        /* drawerLayout = findViewById(R.id.drawer_layout);
@@ -59,43 +48,8 @@ public class MainActivity extends AppCompatActivity {
         actionbar.setDisplayHomeAsUpEnabled(true);
         actionbar.setHomeAsUpIndicator(R.drawable.ic_menu_black_24dp);*/
         loadFragment(new HomeFragment());
-        /*upDataList = new ArrayList<Data>();
-        upRecyclerView = findViewById(R.id.up_recyclerview);
-        GridLayoutManager upGridLayoutManager = new GridLayoutManager(MainActivity.this, 3);
-        upRecyclerView.setLayoutManager(upGridLayoutManager);
-
-        upData =new Data(R.drawable.hackathon,"Hackathon");
-        upDataList.add(upData);
-        upData =new Data(R.drawable.recuritment,"recuritment");
-        upDataList.add(upData);
-        upData =new Data(R.drawable.webvr,"WebVr");
-        upDataList.add(upData);
-
-        DataAdapter upAdapter=new DataAdapter(MainActivity.this, upDataList);
-        upRecyclerView.setAdapter(upAdapter);
-
-        psDataList = new ArrayList<Data>();
-        psRecyclerView = findViewById(R.id.ps_recyclerview);
-        GridLayoutManager psGridLayoutManager = new GridLayoutManager(MainActivity.this, 3);
-        psRecyclerView.setLayoutManager(psGridLayoutManager);
-
-        psData =new Data(R.drawable.github,"OpenSource");
-        psDataList.add(psData);
-        psData =new Data(R.drawable.rust,"Rust");
-        psDataList.add(psData);
-        psData =new Data(R.drawable.googlemini,"Googlemini");
-        psDataList.add(psData);
-
-        DataAdapter psAdapter=new DataAdapter(MainActivity.this, psDataList);
-        psRecyclerView.setAdapter(psAdapter);*/
-
-    }
-    private void loadFragment(Fragment fragment) {
-        // load fragment
-        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-        transaction.replace(R.id.frame_container, fragment);
-        transaction.addToBackStack(null);
-        transaction.commit();
+        BottomNavigationView navigation = findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(MainActivity.this);
     }
    /* @Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -106,4 +60,39 @@ public class MainActivity extends AppCompatActivity {
         }
         return super.onOptionsItemSelected(item);
     }*/
+
+    private boolean loadFragment(Fragment fragment) {
+        //switching fragment
+        if (fragment != null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.fragment_container, fragment)
+                    .commit();
+            return true;
+        }
+        return false;
+    }
+
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        Fragment fragment = null;
+
+        switch (menuItem.getItemId()) {
+            case R.id.nav_home:
+                fragment = new HomeFragment();
+                break;
+
+            case R.id.nav_team:
+                fragment = new TeamFragment();
+                break;
+
+            case R.id.nav_gallery:
+                fragment = new GalleryFragment();
+                break;
+
+        }
+
+        return loadFragment(fragment);
+    }
 }
